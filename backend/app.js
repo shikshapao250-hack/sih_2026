bc94dda8b6611db836ebffb009180732c945d077
jatinkumar2409 <jnkr2409@gmail.com>
2026-09-14 19:29:59 +0530

    added app.js

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+
+import authRoutes from './routes/authRoutes.js';
+import skillsRoutes from './routes/skillsRoute.js';
+import projectsRoutes from './routes/projectsRoutes.js';
+import educationRoutes from './routes/educationRoutes.js';
+import jobsRoutes from './routes/jobRoutes.js';
+import studentsRoutes from './routes/studentsRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/skills', skillsRoutes);
+app.use('/projects', projectsRoutes);
+app.use('/education', educationRoutes);
+app.use('/jobs', jobsRoutes);
+app.use('/students', studentsRoutes);
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+export default app;
+
